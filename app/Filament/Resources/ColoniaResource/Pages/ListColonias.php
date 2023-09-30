1560f2aa6c31859d545875c61619dd0ec933423d
IngCarlosGonzalez <calin_mx@hotmail.com>
2023-09-30 12:47:11 -0600

    Deploy del sabado 30 de septiembre de 2023

--- app/Filament/Resources/ColoniaResource/Pages/ListColonias.php
+++ app/Filament/Resources/ColoniaResource/Pages/ListColonias.php
@@ -16,43 +16,6 @@ class ListColonias extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
-
-            ImportAction::make()
-                ->fields([
-                    ImportField::make('id')
-                    ->label('id'),
-                    ImportField::make('codigo')
-                    ->label('codigo'),
-                    ImportField::make('entidad')
-                    ->label('entidad'),
-                    ImportField::make('municipio_id')
-                    ->label('municipio_id'),
-                    ImportField::make('nombre_mpio')
-                    ->label('nombre_mpio'),
-                    ImportField::make('distrito_fed')
-                    ->label('distrito_fed'),
-                    ImportField::make('distrito_local')
-                    ->label('distrito_local'),
-                    ImportField::make('numero_de_ruta')
-                    ->label('numero_de_ruta'),
-                    ImportField::make('seccion')
-                    ->label('seccion'),
-                    ImportField::make('tipo_seccion')
-                    ->label('tipo_seccion'),
-                    ImportField::make('tipo_colonia')
-                    ->label('tipo_colonia'),
-                    ImportField::make('nombre_colonia')
-                    ->label('nombre_colonia'),
-                    ImportField::make('cod_post_colon')
-                    ->label('cod_post_colon'),
-                    ImportField::make('num_control')
-                    ->label('num_control'),
-                    ImportField::make('created_at')
-                    ->label('created_at'),
-                    ImportField::make('updated_at')
-                    ->label('updated_at'),
-                ])
-
         ];
     }
 
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,14 @@
+@if (filled($brand = filament()->getBrandName()))
+    <div
+        {{
+            $attributes->class([
+                'fi-logo text-xl font-bold leading-5 tracking-tight text-gray-950 dark:text-white',
+            ])
+        }}
+    >
+        <div class="flex flex-row items-center p-2 m-2 space-x-12">
+            <img src="/logos/exotico2.jpg" alt="Logo" class="h-10">
+            &nbsp; Redes-Pueblo
+        </div>
+    </div>
+@endif
